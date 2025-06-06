48c4a28a33b724c0e62134f629cd3c799f9cebb3
Guillem037 <guillemlaragallardo@gmail.com>
2025-06-06 18:26:31 +0200

    cambios en Gustavo.py y Andres.py

--- Andres.py
+++ Andres.py
@@ -1,1 +1,2 @@
 print("Este es el archivo de Andrés")
+print("Guillem modificando el archivo de Andrés")
--- Gustavo.py
+++ Gustavo.py
@@ -1,1 +1,2 @@
 print("Este es el archivo de Gustavo")
+print("Guillem modificando el archivo de Gustavo")
